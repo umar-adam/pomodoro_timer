9d8621f7e1eae5031415078fb1c712ab513f92c4
uadam <149405030+umar-adam@users.noreply.github.com>
2023-10-31 20:00:00 +0000

    Create tomato.py

---
+++ tomato.py
@@ -0,0 +1,3 @@
+# To have a background image of a tomato, just go to the following link:
+# https://commons.wikimedia.org/wiki/File:Fxemoji_u1F345.svg
+# Save the image as 'tomato.png' in the same folder location as the main.py file
