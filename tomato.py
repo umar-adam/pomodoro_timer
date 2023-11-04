addf509aa75144c4e7632a55fe76691e97fa7cd5
uadam <149405030+umar-adam@users.noreply.github.com>
2023-11-04 12:40:15 +0000

    Delete tomato.py

--- tomato.py
+++
@@ -1,3 +0,0 @@
-# To have a background image of a tomato, just go to the following link:
-# https://commons.wikimedia.org/wiki/File:Fxemoji_u1F345.svg
-# Save the image as 'tomato.png' in the same folder location as the main.py file
